07a3aa1c856df67ed6cbfaefc5354a168d6b35b4
debuggor <1915135329@qq.com>
2018-11-19 13:49:31 +0800

    重新换了后台首页样式

---
+++ src/main/java/com/debuggor/mockinterview/common/controller/InterviewTypeController.java
@@ -0,0 +1,13 @@
+package com.debuggor.mockinterview.common.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * IT类别controller
+ */
+@Controller
+@RequestMapping("/admin")
+public class InterviewTypeController {
+
+}
