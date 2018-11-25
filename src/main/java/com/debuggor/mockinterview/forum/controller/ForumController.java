0e08d998a036a0a123f88afc62e622332ad149b3
debuggor <1915135329@qq.com>
2018-11-25 13:28:46 +0800

    新增 论坛静态列表页面

--- src/main/java/com/debuggor/mockinterview/forum/controller/ForumController.java
+++ src/main/java/com/debuggor/mockinterview/forum/controller/ForumController.java
@@ -1,5 +1,14 @@
 package com.debuggor.mockinterview.forum.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/forum")
 public class ForumController {
+
+    @RequestMapping("")
+    public String forumList(){
+        return "front/forum/forum";
+    }
 }
