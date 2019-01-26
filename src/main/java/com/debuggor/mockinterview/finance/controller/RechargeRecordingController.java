5327dbb8e1e490d0fe196a7aee27d65c051a1047
debuggor <1915135329@qq.com>
2019-01-26 13:16:31 +0800

    新增求职者查看充值记录功能

---
+++ src/main/java/com/debuggor/mockinterview/finance/controller/RechargeRecordingController.java
@@ -0,0 +1,42 @@
+package com.debuggor.mockinterview.finance.controller;
+
+import com.debuggor.mockinterview.finance.bean.RechargeRecording;
+import com.debuggor.mockinterview.finance.service.RechargeRecordingService;
+import com.debuggor.mockinterview.interview.bean.Finder;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * 求职者的充值记录
+ */
+@Controller
+@RequestMapping("/recharge")
+public class RechargeRecordingController {
+
+    @Autowired
+    private RechargeRecordingService recordingService;
+
+    /**
+     * 求职者的充值记录历史页面
+     *
+     * @param pn
+     * @param session
+     * @param model
+     * @return
+     */
+    @RequestMapping("/recharges")
+    public String rechargesForFinder(@RequestParam(required = false, defaultValue = "1", value = "pn") Integer pn,
+                                     HttpSession session, Model model) {
+        Finder finder = (Finder) session.getAttribute("finder");
+        model.addAttribute("finder", finder);
+        PageInfo<RechargeRecording> pageInfo = recordingService.getRechargeRecordingsByUid(finder.getFid(), pn);
+        model.addAttribute("pageInfo", pageInfo);
+        return "front/user/finder/recharges";
+    }
+}
--- src/main/java/com/debuggor/mockinterview/finance/service/RechargeRecordingService.java
+++ src/main/java/com/debuggor/mockinterview/finance/service/RechargeRecordingService.java
@@ -1,11 +1,15 @@
 package com.debuggor.mockinterview.finance.service;
 
+import com.debuggor.mockinterview.common.constant.PageConstant;
 import com.debuggor.mockinterview.finance.bean.RechargeRecording;
 import com.debuggor.mockinterview.finance.dao.RechargeRecordingDao;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class RechargeRecordingService {
@@ -21,5 +25,19 @@ public class RechargeRecordingService {
     public void insert(RechargeRecording rechargeRecording) {
         rechargeRecording.setCreateTime(new Date());
         rechargeRecordingDao.insert(rechargeRecording);
+    }
+
+    /**
+     * 得到求职者的充值记录
+     *
+     * @param finderId 求职者ID
+     * @param pn       页数
+     * @return
+     */
+    public PageInfo<RechargeRecording> getRechargeRecordingsByUid(Integer finderId, Integer pn) {
+        PageHelper.startPage(pn, PageConstant.Page_Sizes);
+        List<RechargeRecording> recordings = rechargeRecordingDao.getRechargeRecordingsByUid(finderId);
+        PageInfo<RechargeRecording> pageInfo = new PageInfo<>(recordings, PageConstant.Navigate_Pages);
+        return pageInfo;
     }
 }
